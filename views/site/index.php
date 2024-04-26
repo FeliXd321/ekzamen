5ad7460905da6d90e79c7c76b8e22a81b1d80fbd
Your Name <youremail@yourdomain.com>
2024-04-26 18:21:39 +0300

    Экзамен

---
+++ views/site/index.php
@@ -0,0 +1,118 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'Нарушениям';
+?>
+<main>
+
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2" class="active" aria-current="true"></button>
+      <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3" class=""></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item">
+        <img class="bd-placeholder-img" width="100%" height="100%" src="/web/images/media/0.jpg">
+
+        <div class="container">
+          <div class="carousel-caption text-start">
+            <h1>Добро пожаловать на портал.</h1>
+            <p class="fs-3">Нарушениям.Нет</p>
+            <p><a class="btn btn-lg btn-success" href="http://basic/user/create">Зарегистрироваться</a></p>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item active">
+      <img class="bd-placeholder-img" width="100%" height="100%" src="/web/images/media/12.jpg">
+
+        <div class="container">
+          <div class="carousel-caption">
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+      <img class="bd-placeholder-img" width="100%" height="100%" src="/web/images/media/2.jpg">
+        <div class="container">
+          <div class="carousel-caption text-end">
+          </div>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="text-decoration-underline featurette-heading">Портал 'Нарушениям.Нет'</h2>
+        <p class="fs-3">Портал “Нарушением нет” является передовой платформой по приему заявок о дорожных правонарушениях. Сознательные граждане со всей России оставляют здесь свои заявления, когда не знают, куда обратиться. Каждая обращение граждан доходит до государственной инспекции безопасности дорожного движения (ГИБДД) и проверяется на соответствия всем нормам и правил дорожного движения.</p>
+      </div>
+      <div class="col-md-5">
+        <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src="/web/images/media/1.jpg">
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+      <h2 class=" featurette-heading">О нас</h2>
+        <p class="fs-3">Мы отслеживаем, в каких районах чаще всего происходит правонарушения и передаем сведения в ГИБДД.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+      <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src="/web/images/media/7.jpg">
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <p class="fs-3">Мы следим за вашей безопасностью днем и ночью, давая ответ на заявки в течении пяти-десяти минут после ее формирования.</p>
+      </div>
+      <div class="col-md-5">
+      <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src="/web/images/media/8.jpg">
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <p class="fs-3">Мы контролируем, чтобы каждое одобренное заявление было рассмотрено соответствующим органом.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+      <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src="/web/images/media/6.jpg">
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <p class="fs-3">Мы хотим сделать этот мир лучше с Вами!</p>
+      </div>
+      <div class="col-md-5">
+      <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" src="/web/images/media/10.jpg">
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <!-- /END THE FEATURETTES -->
+
+  </div><!-- /.container -->
